ad306da9582a4cef9dd69cedd2e2167ae2650fbe
Tarun Tiwari <tiwaritarun700@gmail.com>
2025-11-26 18:13:27 +0530

    clean repo

--- src/api/middleware/core.middleware.ts
+++
@@ -1,9 +0,0 @@
-import cors from 'cors';
-import { config } from '../../config';
-
-export const corsMiddleware = cors({
-  origin: config.cors.origin,
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-});
